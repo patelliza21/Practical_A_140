1ebf8b346d53339da7eba9458c35f424c0fb03af
haidaralisunasara429@gmail.com <haidaralisunasara429@gmail.com>
2026-02-24 20:56:09 +0530

    done

--- FullPyramid.java
+++ FullPyramid.java
@@ -1,18 +1,23 @@
 public class FullPyramid {
     public static void main(String[] args) {
+        int n = 5;
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= n; i++) {
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
 
-        for (int i = 1; i <= 5; i++) {
-            int num = 1;
-            for (int j = 1; j <= i; j++) {
-                System.out.print(num + " ");
-                num++;
+        for (int i = n; i >= 1; i--) {
+            for (int s = 1; s <= n - i + 1; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print((char) ('A' + j) + " ");
             }
             System.out.println();
         }
